789571552927ea43b8551732ba82533c35ea7e0a
verdvana <739197909@qq.com>
2026-07-16 15:57:22 +0800

    Refactor code structure for improved readability and maintainability

--- transfer/design_20260707_update/sdc/smmu_rst.tcl
+++
@@ -1,7 +0,0 @@
-
-#========================================
-#Reset Constraint
-set_ideal_network [get_port rst_n]
-set_dont_touch_network [get_port rst_n]
-set_drive 0 [get_port rst_n]
-set_false_path -from [get_port rst_n]
